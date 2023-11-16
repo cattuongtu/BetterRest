080db1533224829db822239c583e6c7e7bb8b6f1
cattuongtu <cattuongtu3@gmail.com>
2023-11-15 17:19:39 -0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cat-Tuong Tu on 11/15/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
